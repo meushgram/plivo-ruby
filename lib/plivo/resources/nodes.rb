6d71b97e2fd2176839c3b82a51a53bb75e2371b9
Koushik-Ayila <koushik.ayila@plivo.com>
2020-07-07 13:47:04 +0530

    Retracted the names back to original

--- lib/plivo/resources/nodes.rb
+++ lib/plivo/resources/nodes.rb
@@ -15,7 +15,7 @@ module Plivo
       def configure_node_type(node_type)
         case node_type
         when 'multi_party_call'
-          MultiPartyCalls
+          MultiPartyCall
         # when 'conference_bridge'
         #   ConferenceBridge
         end
@@ -51,7 +51,7 @@ module Plivo
       end
     end
 
-    class MultiPartyCalls < Node
+    class MultiPartyCall < Node
       def initialize(client,options=nil)
         @_name = 'multi_party_call'
         super
